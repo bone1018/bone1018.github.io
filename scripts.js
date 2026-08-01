e7b36005eb07f5c9b40e8adff4b7cdd1a7a0dbba
bone1018 <myself961018@gmail.com>
2026-08-01 17:16:44 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -77,9 +77,6 @@ document.addEventListener('DOMContentLoaded', () => {
             audio.volume = Number(volumeSlider.value) / 100;
         });
 
-        audio.addEventListener('play', () => photoPiece.classList.remove('paused'));
-        audio.addEventListener('pause', () => photoPiece.classList.add('paused'));
-
         audio.addEventListener('play', updateIcon);
         audio.addEventListener('pause', updateIcon);
     }
